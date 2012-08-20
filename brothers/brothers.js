872620f74d563b3fe7ddba96e7bef6ebf1017645
George Erickson <george55@mit.edu>
2012-08-19 21:01:25 -0400

    making brothers dynamically load

--- brothers/brothers.js
+++ brothers/brothers.js
@@ -1,1 +1,274 @@
-window.brothers = [{"interests": " Golf, P. Diddy", "name": "Nick Swenson", "hometown": " Yorktown, VA", "course": " 20", "year": " 2012", "aspirations": " Bain and Co. Consulting, Stanford Med"}, {"interests": " Electro, Trance, Concerts", "name": "Joshua Leonard-Goodwill", "hometown": " Rancho Cucamunga", "course": " 8", "year": " 2012", "aspirations": " Trading"}, {"interests": " Electro, Dubstep, DJing", "name": "Adam Delton", "hometown": " Fresno, CA", "course": " 2", "year": " 2012", "aspirations": " Tesla Motors Engineer"}, {"interests": " Lead-Singer of Transit Gloria", "name": "Stephen Goodman", "hometown": " San Diego, CA", "course": " 8, 18", "year": " 2012", "aspirations": " Bain and Co. Consulting, Private Equity"}, {"interests": " Diving, Swimming, Krillin", "name": "Chao He", "hometown": " Newton, MA", "course": " 2", "year": " 2012", "aspirations": " Rest Devices Startup"}, {"interests": " Snowboarding, Cycling", "name": "Colin Reisterer", "hometown": " Anchorage, AK", "course": " 20", "year": " 2012", "aspirations": " Biomedical Engineering"}, {"interests": " Lacrosse, Golf", "name": "Michael Bickers", "hometown": " Charlotte, NC", "course": " 2", "year": " 2012", "aspirations": " Pelton & Crane Engineer"}, {"interests": " Coding, Developing", "name": "Neel Hajare", "hometown": " Boston, MA", "course": " 6", "year": " 2012", "aspirations": " Quora, Facebook developer"}, {"interests": " Wrestling, Live music, Donating to Locks of Love", "name": "Hank Peppe", "hometown": " Omaha, NE", "course": " 2", "year": " 2012", "aspirations": " Multi-national charity organization"}, {"interests": " Bowties, cycling, swimming, and economics", "name": "Trace Sauter", "hometown": " Philadelphia, PA", "course": " 14", "year": " 2012", "aspirations": " Risk Management Banking at Bank of America"}, {"interests": " Kinesiology, A Cappella, Tennis", "name": "Brian Wee", "hometown": " Woodbury, NY", "course": " 3", "year": " 2012", "aspirations": " Materials Selection & Product Development"}, {"interests": " Fencing, Traveling", "name": "Jackson Crane", "hometown": " San Diego, CA", "course": " 2", "year": " 2013", "aspirations": " Alternative Energy"}, {"interests": " Electro", "name": "Pat Marx", "hometown": " Grand Rapids, MO", "course": " 6", "year": " 2013", "aspirations": " Google, or Startup"}, {"interests": " EDM, Technology", "name": "Albert Rodenius", "hometown": " Sacramento, CA", "course": " 6", "year": " 2013", "aspirations": " Software"}, {"interests": " EDM, Concerts, Philosophy", "name": "Charles Huang", "hometown": " Las Vegas, NV", "course": " 3", "year": " 2013", "aspirations": " Research"}, {"interests": " Soccer, MIT Energy Club,", "name": "Scott Jenkins", "hometown": " Amherst, MA", "course": " 10", "year": " 2013", "aspirations": " Alternative energy"}, {"interests": " Web developing, Sriracha sauce", "name": "George Edward Erickson Jr.", "hometown": " Jacksonville, FL", "course": " 6", "year": " 2013", "aspirations": " Web developer"}, {"interests": " LA disco, French electro, German techno, British rock, info art", "name": "Alex Teuffer Pando", "hometown": " Miami, FL", "course": " 2", "year": " 2013", "aspirations": " SpaceX, build/design machines that go really fast"}, {"interests": " Being British, Mate", "name": "Jack Newman", "hometown": " London, UK", "course": " 8", "year": " 2013", "aspirations": " To become the next Queen"}, {"interests": " Ice cream, EDM, Concerts", "name": "Jason Elizalde", "hometown": " Corpus Christi, TX", "course": " 16", "year": " 2013", "aspirations": " SpaceX"}, {"interests": " Robots, Rock", "name": "David Benhaim", "hometown": " Boston, MA", "course": " 6", "year": " 2013", "aspirations": " Raytheon Engineer"}, {"interests": " Indie music, Physics, Travel,", "name": "Christopher Schuch", "hometown": " Colorado Springs, CO", "course": " 8", "year": " 2013", "aspirations": " Goldman Sachs, PhD in physics"}, {"interests": " Football, Sign language, History", "name": "Max Powers", "hometown": " Rochester, NY", "course": " 3", "year": " 2014", "aspirations": " Materials research"}, {"interests": " Computers, Open-sourceware, Politics, Soccer", "name": "Charlie Gordon", "hometown": " New York City", "course": " 6", "year": " 2014", "aspirations": " Google"}, {"interests": " Football, Music", "name": "Kevin Clayton", "hometown": " Potomac, MD", "course": " 10", "year": " 2014", "aspirations": " Chemical Engineering, Pharmaceutical Engineering"}, {"interests": " Indie-rock, Hipster stuff,", "name": "Rick Zang", "hometown": " Pasadena, CA", "course": " 2", "year": " 2014", "aspirations": " NASA"}, {"interests": " Track, Renewable Energy, Outdoorsy stuff", "name": "Mac Gager", "hometown": " Rye, NY", "course": " 3", "year": " 2014", "aspirations": " Materials Engineering, Start a company"}, {"interests": " Magnets, Playing night crawlers, Skitchin\u2019", "name": "Zev Bimstein", "hometown": " San Diego, CA", "course": " 14, 15", "year": " 2015", "aspirations": " Management Consulting, Venture capitalism, Be a live audience member at SNL"}, {"interests": " Skiing, EDM", "name": "Mikey Suglian", "hometown": " Lake Tahoe, CA", "course": " 3", "year": " 2015", "aspirations": " Carbon nano-tube engineering"}, {"interests": " EDM, Piano", "name": "Jasper Lienhard", "hometown": " Lexington, MA", "course": " 3", "year": " 2015", "aspirations": " Materials Engineer"}, {"interests": " Crew, Coding", "name": "Chris Kim", "hometown": " Dallas, TX", "course": " 6", "year": " 2015", "aspirations": " Coder for Apple"}, {"interests": " EDM, Bodyboarding, Photography, Art", "name": "Brandon Le", "hometown": " San Diego, CA", "course": " 16", "year": " 2015", "aspirations": " Trading, Aerospace Engineering"}, {"interests": " Swimming, cave-dwelling", "name": "Thomas Finn", "hometown": " Potomac, MD", "course": " 2", "year": " 2015", "aspirations": " Nuclear engineering"}, {"interests": " Intervarsity, long boarding, concept cars, graffiti, and extreme sports", "name": "Parker Chambers", "hometown": " Colorado Springs, CO", "course": " 2", "year": " 2015", "aspirations": " To design a new, more practical, and safer vehicles"}];
+window.brothers = [
+    {
+        "aspirations": "Bain and Co. Consulting, Stanford Med", 
+        "course": "20", 
+        "hometown": "Yorktown, VA", 
+        "interests": "Golf, P. Diddy", 
+        "name": "Nick Swenson", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Trading", 
+        "course": "8", 
+        "hometown": "Rancho Cucamunga", 
+        "interests": "Electro, Trance, Concerts", 
+        "name": "Joshua Leonard-Goodwill", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Tesla Motors Engineer", 
+        "course": "2", 
+        "hometown": "Fresno, CA", 
+        "interests": "Electro, Dubstep, DJing", 
+        "name": "Adam Delton", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Bain and Co. Consulting, Private Equity", 
+        "course": "8, 18", 
+        "hometown": "San Diego, CA", 
+        "interests": "Lead-Singer of Transit Gloria", 
+        "name": "Stephen Goodman", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Rest Devices Startup", 
+        "course": "2", 
+        "hometown": "Newton, MA", 
+        "interests": "Diving, Swimming, Krillin", 
+        "name": "Chao He", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Biomedical Engineering", 
+        "course": "20", 
+        "hometown": "Anchorage, AK", 
+        "interests": "Snowboarding, Cycling", 
+        "name": "Colin Reisterer", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Pelton & Crane Engineer", 
+        "course": "2", 
+        "hometown": "Charlotte, NC", 
+        "interests": "Lacrosse, Golf", 
+        "name": "Michael Bickers", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Quora, Facebook developer", 
+        "course": "6", 
+        "hometown": "Boston, MA", 
+        "interests": "Coding, Developing", 
+        "name": "Neel Hajare", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Multi-national charity organization", 
+        "course": "2", 
+        "hometown": "Omaha, NE", 
+        "interests": "Wrestling, Live music, Donating to Locks of Love", 
+        "name": "Hank Peppe", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Risk Management Banking at Bank of America", 
+        "course": "14", 
+        "hometown": "Philadelphia, PA", 
+        "interests": "Bowties, cycling, swimming, and economics", 
+        "name": "Trace Sauter", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Materials Selection & Product Development", 
+        "course": "3", 
+        "hometown": "Woodbury, NY", 
+        "interests": "Kinesiology, A Cappella, Tennis", 
+        "name": "Brian Wee", 
+        "year": 2012
+    }, 
+    {
+        "aspirations": "Alternative Energy", 
+        "course": "2", 
+        "hometown": "San Diego, CA", 
+        "interests": "Fencing, Traveling", 
+        "name": "Jackson Crane", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "Google, or Startup", 
+        "course": "6", 
+        "hometown": "Grand Rapids, MO", 
+        "interests": "Electro", 
+        "name": "Pat Marx", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "Software", 
+        "course": "6", 
+        "hometown": "Sacramento, CA", 
+        "interests": "EDM, Technology", 
+        "name": "Albert Rodenius", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "Research", 
+        "course": "3", 
+        "hometown": "Las Vegas, NV", 
+        "interests": "EDM, Concerts, Philosophy", 
+        "name": "Charles Huang", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "Alternative energy", 
+        "course": "10", 
+        "hometown": "Amherst, MA", 
+        "interests": "Soccer, MIT Energy Club,", 
+        "name": "Scott Jenkins", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "Web developer", 
+        "course": "6", 
+        "hometown": "Jacksonville, FL", 
+        "interests": "Web developing, Sriracha sauce", 
+        "name": "George Edward Erickson Jr.", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "SpaceX, build/design machines that go really fast", 
+        "course": "2", 
+        "hometown": "Miami, FL", 
+        "interests": "LA disco, French electro, German techno, British rock, info art", 
+        "name": "Alex Teuffer Pando", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "To become the next Queen", 
+        "course": "8", 
+        "hometown": "London, UK", 
+        "interests": "Being British, Mate", 
+        "name": "Jack Newman", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "SpaceX", 
+        "course": "16", 
+        "hometown": "Corpus Christi, TX", 
+        "interests": "Ice cream, EDM, Concerts", 
+        "name": "Jason Elizalde", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "Raytheon Engineer", 
+        "course": "6", 
+        "hometown": "Boston, MA", 
+        "interests": "Robots, Rock", 
+        "name": "David Benhaim", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "Goldman Sachs, PhD in physics", 
+        "course": "8", 
+        "hometown": "Colorado Springs, CO", 
+        "interests": "Indie music, Physics, Travel,", 
+        "name": "Christopher Schuch", 
+        "year": 2013
+    }, 
+    {
+        "aspirations": "Materials research", 
+        "course": "3", 
+        "hometown": "Rochester, NY", 
+        "interests": "Football, Sign language, History", 
+        "name": "Max Powers", 
+        "year": 2014
+    }, 
+    {
+        "aspirations": "Google", 
+        "course": "6", 
+        "hometown": "New York City", 
+        "interests": "Computers, Open-sourceware, Politics, Soccer", 
+        "name": "Charlie Gordon", 
+        "year": 2014
+    }, 
+    {
+        "aspirations": "Chemical Engineering, Pharmaceutical Engineering", 
+        "course": "10", 
+        "hometown": "Potomac, MD", 
+        "interests": "Football, Music", 
+        "name": "Kevin Clayton", 
+        "year": 2014
+    }, 
+    {
+        "aspirations": "NASA", 
+        "course": "2", 
+        "hometown": "Pasadena, CA", 
+        "interests": "Indie-rock, Hipster stuff,", 
+        "name": "Rick Zang", 
+        "year": 2014
+    }, 
+    {
+        "aspirations": "Materials Engineering, Start a company", 
+        "course": "3", 
+        "hometown": "Rye, NY", 
+        "interests": "Track, Renewable Energy, Outdoorsy stuff", 
+        "name": "Mac Gager", 
+        "year": 2014
+    }, 
+    {
+        "aspirations": "Management Consulting, Venture capitalism, Be a live audience member at SNL", 
+        "course": "14, 15", 
+        "hometown": "San Diego, CA", 
+        "interests": "Magnets, Playing night crawlers, Skitchin\u2019", 
+        "name": "Zev Bimstein", 
+        "year": 2015
+    }, 
+    {
+        "aspirations": "Carbon nano-tube engineering", 
+        "course": "3", 
+        "hometown": "Lake Tahoe, CA", 
+        "interests": "Skiing, EDM", 
+        "name": "Mikey Suglian", 
+        "year": 2015
+    }, 
+    {
+        "aspirations": "Materials Engineer", 
+        "course": "3", 
+        "hometown": "Lexington, MA", 
+        "interests": "EDM, Piano", 
+        "name": "Jasper Lienhard", 
+        "year": 2015
+    }, 
+    {
+        "aspirations": "Coder for Apple", 
+        "course": "6", 
+        "hometown": "Dallas, TX", 
+        "interests": "Crew, Coding", 
+        "name": "Chris Kim", 
+        "year": 2015
+    }, 
+    {
+        "aspirations": "Trading, Aerospace Engineering", 
+        "course": "16", 
+        "hometown": "San Diego, CA", 
+        "interests": "EDM, Bodyboarding, Photography, Art", 
+        "name": "Brandon Le", 
+        "year": 2015
+    }, 
+    {
+        "aspirations": "Nuclear engineering", 
+        "course": "2", 
+        "hometown": "Potomac, MD", 
+        "interests": "Swimming, cave-dwelling", 
+        "name": "Thomas Finn", 
+        "year": 2015
+    }, 
+    {
+        "aspirations": "To design a new, more practical, and safer vehicles", 
+        "course": "2", 
+        "hometown": "Colorado Springs, CO", 
+        "interests": "Intervarsity, long boarding, concept cars, graffiti, and extreme sports", 
+        "name": "Parker Chambers", 
+        "year": 2015
+    }
+];
--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,5 +1,28 @@
-$(document).ready ->
+#convert underscore to use mustache template
+_.templateSettings = {
+  interpolate : /\{\{(.+?)\}\}/g
+};
+
+class BrothersView extends Backbone.View
+  initialize: ->
+    #Only include brothers for this year
+    @brothers = _.filter window.brothers, (b) =>
+      b.year is @options.year
+    
+    @template = Handlebars.compile ($ '#brother-detail-template').html()
+    
+    @render()
   
+  render: ->
+    @$el.html @template
+      brothers: @brothers
+
+
+
+$(document).ready ->
   #we don't want the carousel automatically cycling
-  ($ '.carousel').carousel
-    interval: false
+  ($ '.carousel').carousel 'pause'
+  
+  new BrothersView
+    el: '#bro-2012-details'
+    year: 2012 
